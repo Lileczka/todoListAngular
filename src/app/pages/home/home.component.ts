b99c266c4725667623d23069b85e2a62e4f4b113
Lilianna <liliana.guderska@gmail.com>
2023-04-11 22:03:28 +0200

    page 2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,82 +6,101 @@ import { TodoService } from 'src/app/todo.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   todos: ITodo[] = ITODOS;
-  label= "Créer une tache";
+  label = 'Créer une tache';
   showPage = false;
   // Déclarer les variables pour stocker la liste d'éléments urgents
-  urgentList: { id: number, task: string, isUrgent: boolean }[] = [];
-  ordinaryList: { id: number, task: string, isUrgent: boolean }[] = [];
+  urgentList: { id: number; task: string; isUrgent: boolean }[] = [];
+  ordinaryList: { id: number; task: string; isUrgent: boolean }[] = [];
   //pour modifier le task
   modifiedTask: string = '';
-  
-  constructor(private todoService: TodoService, 
+
+  constructor(
+    private todoService: TodoService,
     private router: Router,
-    private route: ActivatedRoute) {}
-    
-    ngOnInit(): void {
- // Récupérer la liste des éléments  du localStorage
- this.urgentList = JSON.parse(localStorage.getItem('urgentList') || '[]');
- this.ordinaryList = JSON.parse(localStorage.getItem('ordinaryList') || '[]');
+    private route: ActivatedRoute
+  ) {}
 
- //afficher la pertie caché de ng-container
- this.route.queryParams.subscribe(params => {
-  this.showPage = params['showPage'] === 'false';
-});
-}
- 
-//passer la valeur de showPage à true pour cacher la partie selection
-afficherSelectedTask() {
-  this.showPage = !this.showPage;
-  localStorage.setItem('showPage', JSON.stringify(this.showPage));
-}
+  ngOnInit(): void {
+    // Récupérer la liste des éléments  du localStorage
+    this.urgentList = JSON.parse(localStorage.getItem('urgentList') || '[]');
+    this.ordinaryList = JSON.parse(localStorage.getItem('ordinaryList') || '[]'
+    );
 
-goToPage2() {
-  this.router.navigate(['/page2']);
-  
-  }
-goToPage3() {
-    
-    this.router.navigate(['/page3']); 
+    //afficher la pertie caché de ng-container
+    this.route.queryParams.subscribe((params) => {
+      this.showPage = params['showPage'] === 'false';
+    });
   }
 
-removeTask(task: {id: number, task: string}, listType: string): void {
+  //passer la valeur de showPage à true pour cacher la partie selection
+  afficherSelectedTask() {
+    this.showPage = !this.showPage;
+    localStorage.setItem('showPage', JSON.stringify(this.showPage));
+  }
+
+  goToPage2() {
+    this.router.navigate(['/page2']);
+  }
+  goToPage3() {
+    this.router.navigate(['/page3']);
+  }
+
+  removeTask(task: { id: number; task: string }, listType: string): void {
     this.todoService.removeTask(task, listType);
     // Navigate to page 3
-     this.goToPage3();
-   // Ajouter la tâche enlevée à historyList, méthode appelé du service
-     this.todoService.addToHistoryList(task);
+    this.goToPage3();
+    // Ajouter la tâche enlevée à historyList, méthode appelé du service
+    this.todoService.addToHistoryList(task);
   }
-  
+
   getmodifiedTask(task: string): void {
     // Supprimer la tâche de la liste 'urgentList'
-  const indexUrgent = this.todoService.isUrgent.findIndex(t => t.task === task);
-  if (indexUrgent !== -1) {
-    this.todoService.isUrgent.splice(indexUrgent, 1);
-    localStorage.setItem('urgentList', JSON.stringify(this.todoService.isUrgent));
-  }
- // Supprimer la tâche de la liste 'ordinaryList'
- const indexOrdinary = this.todoService.isOrdinary.findIndex(t => t.task === task);
- if (indexOrdinary !== -1) {
-   this.todoService.isOrdinary.splice(indexOrdinary, 1);
-   localStorage.setItem('ordinaryList', JSON.stringify(this.todoService.isOrdinary));
- }
- 
-  
- this.modifiedTask = task;
-  
- let queryParams: NavigationExtras = {
+    const indexUrgent = this.todoService.isUrgent.findIndex(
+      (t) => t.task === task
+    );
+    if (indexUrgent !== -1) {
+      const removedUrgentTask = this.todoService.isUrgent.splice(
+        indexUrgent,
+        1
+      )[0];
+      localStorage.setItem(
+        'urgentList',
+        JSON.stringify(this.todoService.isUrgent)
+      );
+      // Stocker la valeur de 'isUrgent' dans le local storage
+      localStorage.setItem('state', JSON.stringify(removedUrgentTask));
+    }
+
+    // Supprimer la tâche de la liste 'ordinaryList'
+    const indexOrdinary = this.todoService.isOrdinary.findIndex(
+      (t) => t.task === task
+    );
+    if (indexOrdinary !== -1) {
+      const removedOrdinaryTask = this.todoService.isOrdinary.splice(
+        indexOrdinary,
+        1
+      )[0];
+      localStorage.setItem(
+        'ordinaryList',
+        JSON.stringify(this.todoService.isOrdinary)
+      );
+      // Stocker la valeur de 'isUrgent' dans le local storage
+      localStorage.setItem('state', JSON.stringify(removedOrdinaryTask));
+    }
+
+    this.modifiedTask = task;
+
+    let queryParams: NavigationExtras = {
       queryParams: {
-      task: this.modifiedTask
-      }
+        task: this.modifiedTask,
+      },
     };
     // Naviguer vers la page 2 avec le task comme paramètre de requête
-    this.router.navigate(['/page2'], queryParams); 
-    return; 
+    this.router.navigate(['/page2'], queryParams);
+    return;
   }
- }
-
-  
+}
--- src/app/pages/page2/page2.component.ts
+++ src/app/pages/page2/page2.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
-  styleUrls: ['./page2.component.css']
+  styleUrls: ['./page2.component.css'],
 })
 export class Page2Component implements OnInit {
   todos: ITodo[] = ITODOS;
@@ -17,117 +17,128 @@ export class Page2Component implements OnInit {
   isInputEmpty = false;
   newTask!: string;
   showPage = false;
-  
-  label= "Valider une tache";
-  
+
+  label = 'Valider une tache';
+
   modifiedTask: string = '';
-  
-constructor(public todoService: TodoService, 
+  isUrgent = false;
+  constructor(
+    public todoService: TodoService,
     private router: Router,
-    private route: ActivatedRoute) {
-     
-  // garantir que tous les éléments soient désactivés par défaut avant interaction avec eux
-  this.todos.forEach(todo => todo.isActive = false);
+    private route: ActivatedRoute
+  ) {
+    // garantir que tous les éléments soient désactivés par défaut avant interaction avec eux
+    this.todos.forEach((todo) => (todo.isActive = false));
   }
-  
+
   //afficher task à modifier
   ngOnInit(): void {
     //recuperer task de la page2
     const task = this.route.snapshot.queryParamMap.get('task');
     //enlever emoji de task
     if (task) {
-    // Extraire toutes les chaînes de caractères de la tâche
-    const taskParts = task.split(" ");
-    // Enlever l'emoji en retirant la première partie
-    taskParts.shift();
-    // Joindre les parties restantes pour obtenir la nouvelle tâche
-    this.newTask = taskParts.join(" ");
-    // Mettre à jour modifiedTask avec newTask
-    this.modifiedTask = this.newTask;
-  // extraire emoji 
-  const taskContent = task.split(" ")[0];
-  // mettre emoji dans local storage
-  localStorage.setItem('taskContent', taskContent); 
-    
-    
-
-   
-// Activer le bouton selecteur
-if (taskContent) {
-  this.modifiedTask = taskContent;
-  this.todos.forEach(todo => {
-    if (todo.content === taskContent) {
-      if (this.activeTodo === todo) {
-        todo.isActive = false;
-        this.activeTodo = null;
-      } else {
-        this.todos.forEach(t => t.isActive = false);
-        todo.isActive = true;
-        this.activeTodo = todo;
+      // Extraire toutes les chaînes de caractères de la tâche
+      const taskParts = task.split(' ');
+      // Enlever l'emoji en retirant la première partie
+      taskParts.shift();
+      // Joindre les parties restantes pour obtenir la nouvelle tâche
+      this.newTask = taskParts.join(' ');
+      // Mettre à jour modifiedTask avec newTask
+      this.modifiedTask = this.newTask;
+      // extraire emoji
+      const taskContent = task.split(' ')[0];
+      // mettre emoji dans local storage
+      localStorage.setItem('taskContent', taskContent);
+      
+      const isUrgent = localStorage.getItem('state') === 'true';
+      console.log(isUrgent);
+      this.isActive = isUrgent;
+      
+      // Activer le bouton selecteur
+      if (taskContent) {
+        this.modifiedTask = taskContent;
+        this.todos.forEach((todo) => {
+          if (todo.content === taskContent) {
+            if (this.activeTodo === todo) {
+              todo.isActive = false;
+              this.activeTodo = null;
+            } else {
+              this.todos.forEach((t) => (t.isActive = false));
+              todo.isActive = true;
+              this.activeTodo = todo;
+            }
+          } else {
+            todo.isActive = false;
+          }
+        });
       }
-    } else {
-      todo.isActive = false;
     }
-  });
-}
-}
-  this.activerBtnFullInput();
-    
+    this.activerBtnFullInput();
+    this.btnCheckActiveModif();
     this.goToPage2();
-    
   }
-  
+
   goToPage2() {
     this.router.navigate(['/page2']);
-    
   }
-  
+
   btnActive(todo: ITodo) {
     if (this.activeTodo === todo) {
       todo.isActive = false;
       this.activeTodo = null;
     } else {
-      this.todos.forEach(t => t.isActive = false);
+      this.todos.forEach((t) => (t.isActive = false));
       todo.isActive = true;
       this.activeTodo = todo;
     }
   }
-  
+
   saveTask(): void {
     if (this.isInputEmpty) {
       //  ajouter emoji à la nouvelle tâche
-     this.newTask = this.activeTodo?.content ? this.activeTodo.content + ' ' + this.newTask : this.newTask;
+      this.newTask = this.activeTodo?.content
+        ? this.activeTodo.content + ' ' + this.newTask
+        : this.newTask;
       // Appeler le service pour sauvegarder la nouvelle tâche
-     this.todoService.saveTask(this.newTask, this.isActive);
-       // Réinitialiser l'input et la valeur d'activation du bouton
-     this.newTask = '';
-     this.isActive = false;
- } 
- 
-  console.log('Liste urgente:');
-  this.todoService.isUrgent.forEach(task => {
-    console.log(`Tâche ${task.id}: ${task.task}`);
-  });
-  
-  console.log('Liste ordinaire:');
-  this.todoService.isOrdinary.forEach(task => {
-    console.log(`Tâche ${task.id}: ${task.task}`);
-  });
-  this.goPageHome();
-  
-  } 
-//activer un bouton lorsque on saisi du texte dans un champ d'entrée (input).
-activerBtnFullInput() {
-  this.isInputEmpty = this.newTask?.trim().length > 0;
+      this.todoService.saveTask(this.newTask, this.isActive);
+      // Réinitialiser l'input et la valeur d'activation du bouton
+      this.newTask = '';
+      this.isActive = false;
+    }
+
+    console.log('Liste urgente:');
+    this.todoService.isUrgent.forEach((task) => {
+      console.log(`Tâche ${task.id}: ${task.task}`);
+    });
+
+    console.log('Liste ordinaire:');
+    this.todoService.isOrdinary.forEach((task) => {
+      console.log(`Tâche ${task.id}: ${task.task}`);
+    });
+    this.goPageHome();
+  }
+  //activer un bouton lorsque on saisi du texte dans un champ d'entrée (input).
+  activerBtnFullInput() {
+    this.isInputEmpty = this.newTask?.trim().length > 0;
   }
 
   //buton selection isurgent
-btnCheckActive() {
+  btnCheckActive() {
     this.isActive = !this.isActive;
   }
-  
 
-//recuperer la valeur true de showPage pour changer l'affichage de la page1
+  btnCheckActiveModif() {
+    const state = localStorage.getItem('state');
+    if (state) {
+      const task = JSON.parse(state);
+      if (task.isUrgent) {
+        this.isUrgent = true;
+        this.isActive = true; // activate the button
+      }
+    }
+  }
+
+  //recuperer la valeur true de showPage pour changer l'affichage de la page1
   goPageHome(): void {
     const storedShowPage = localStorage.getItem('showPage');
     const showPage = storedShowPage ? JSON.parse(storedShowPage) : false;
